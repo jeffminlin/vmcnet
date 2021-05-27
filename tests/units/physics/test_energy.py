4da703c27ac6475992a23cae57fe8d27ac13bf1c
Jeffmin Lin <jeffmin.lin@gmail.com>
2021-05-27 01:52:20 -0700

    adding local laplacian and harmonic osc potential

---
+++ tests/units/physics/test_energy.py
@@ -0,0 +1,60 @@
+"""Testing energy calculations."""
+import functools
+import jax
+import jax.numpy as jnp
+import numpy as np
+
+import vmcnet.physics as physics
+
+
+def _make_dummy_log_f():
+    f = lambda _, x: jnp.sum(jnp.square(x) + 3 * x)
+    log_f = lambda _, x: jnp.log(jnp.abs(f(_, x)))
+    return f, log_f
+
+
+def _make_dummy_x():
+    return jnp.array([[1.0], [2.0], [3.0]])
+
+
+def test_laplacian_psi_over_psi():
+    """Test (nabla^2 f)(x) / f(x) for f(x) = sum_i x_i^2 + 3x_i."""
+    f, log_f = _make_dummy_log_f()
+    x = _make_dummy_x()
+
+    grad_log_f = jax.grad(log_f, argnums=1)
+
+    local_laplacian = physics.energy.laplacian_psi_over_psi(grad_log_f, None, x)
+
+    np.testing.assert_allclose(local_laplacian, 3 * 2 / f(None, x), rtol=1e-6)
+
+
+def test_make_harmonic_oscillator_local_energy_with_zero_omega():
+    """Test the creation of the harmonic oscillator local energy with omega = 0."""
+    f, log_f = _make_dummy_log_f()
+    vmapped_f = jax.vmap(f, in_axes=(None, 0), out_axes=0)
+    multichain_x = jnp.reshape(jnp.arange(12, dtype=jnp.float32), (4, 3, 1))
+
+    local_energy_fn = physics.energy.make_harmonic_oscillator_local_energy(0.0, log_f)
+
+    kinetic = local_energy_fn(None, multichain_x)  # kinetic only because omega = 0
+    expected = jnp.expand_dims(-3.0 / vmapped_f(None, multichain_x), axis=-1)
+
+    np.testing.assert_allclose(kinetic, expected, rtol=1e-6)
+
+
+def test_make_harmonic_oscillator_local_energy_with_nonzero_omega():
+    """Test the creation of the harmonic oscillator local energy with omega = 1.0."""
+    f, log_f = _make_dummy_log_f()
+    vmapped_f = jax.vmap(f, in_axes=(None, 0), out_axes=0)
+    multichain_x = jnp.reshape(jnp.arange(12, dtype=jnp.float32), (4, 3, 1))
+
+    omega = 2.0
+
+    local_energy_fn = physics.energy.make_harmonic_oscillator_local_energy(omega, log_f)
+
+    local_energy = local_energy_fn(None, multichain_x)
+    kinetic = jnp.expand_dims(-3.0 / vmapped_f(None, multichain_x), axis=-1)
+    potential = 0.5 * jnp.sum(jnp.square(omega * multichain_x), axis=-2)
+
+    np.testing.assert_allclose(local_energy, kinetic + potential, rtol=1e-6)
---
+++ vmcnet/physics/energy.py
@@ -0,0 +1,112 @@
+"""Local energy calculations."""
+from typing import Callable, TypeVar
+
+import jax
+from jax._src.api import grad
+import jax.numpy as jnp
+
+P = TypeVar("P")  # represents a pytree or pytree-like object containing model params
+
+
+def laplacian_psi_over_psi(
+    grad_log_psi: Callable[[P, jnp.ndarray], jnp.ndarray], params: P, x: jnp.ndarray
+) -> jnp.ndarray:
+    """Compute (nabla^2 psi) / psi at x given a function which evaluates psi'(x)/psi.
+
+    The computation is done by computing (forward-mode) derivatives of the gradient to
+    get the columns of the Hessian, and accumulating the (i, i)th entries (but this
+    implementation is significantly more memory efficient than directly computing the
+    Hessian).
+
+    This function uses the identity
+
+        (nabla^2 psi) / psi = (nabla^2 log|psi|) + (nabla log|psi|)^2
+
+    to avoid leaving the log domain during the computation.
+
+    This function should be vmapped in order to be applied to batches of inputs, as it
+    completely flattens x in order to take second derivatives w.r.t. each component.
+
+    This is approach is extremely similar to the one in the FermiNet repo
+    (in the jax branch, as of this writing -- see
+    https://github.com/deepmind/ferminet/blob/aade61b3d30883b3238d6b50c85404d0e8176155/ferminet/hamiltonian.py).
+
+    The main difference is that we are being explicit about the flattening of x within
+    the Laplacian calculation, so that it does not have to be handled outside of this
+    function (psi is free to take x shapes which are not flat).
+
+    Args:
+        grad_log_psi (Callable): function which evaluates the derivative of log|psi(x)|,
+            i.e. (nabla psi)(x) / psi(x), with respect to x. Has the signature
+            (params, x) -> (nabla psi)(x) / psi(x), so the derivative should be over the
+            second arg, x, and the output shape should be the same as x
+        params (pytree): model parameters, passed as the first arg of grad_log_psi
+        x (jnp.ndarray): second input to grad_log_psi
+
+    Returns:
+        jnp.ndarray: "local" laplacian calculation, i.e. (nabla^2 psi) / psi
+    """
+    x_shape = x.shape
+    flat_x = jnp.reshape(x, (-1,))
+    n = flat_x.shape[0]
+    identity_mat = jnp.eye(n)
+
+    grad_log_psi_of_flat_x = lambda flat_in: grad_log_psi(
+        params, jnp.reshape(flat_in, x_shape)
+    )
+
+    def step_fn(carry, unused):
+        del unused
+        i = carry[0]
+        primals, tangents = jax.jvp(
+            grad_log_psi_of_flat_x, (flat_x,), (identity_mat[i],)
+        )
+        return (i + 1, carry[1] + primals[i] ** 2 + tangents[i]), None
+
+    out, _ = jax.lax.scan(step_fn, (0, jnp.array([0.0])), xs=None, length=n)
+    return out[1]
+
+
+def harmonic_oscillator_potential(omega: jnp.float32, x: jnp.ndarray) -> jnp.float32:
+    """Potential energy for independent harmonic oscillators with spring constant omega.
+
+    This function computes sum_i 0.5 * (omega * x_i)^2. If x has more than one axis,
+    these are simply summed over, so this corresponds to an isotropic harmonic
+    oscillator potential.
+
+    This function should be vmapped in order to be applied to batches of inputs, as it
+    expects the first axis of x to correspond to the particle index.
+
+    Args:
+        omega (jnp.float32): spring constant
+        x (jnp.ndarray): array of particle positions, where the first axis corresponds
+            to particle index
+
+    Returns:
+        jnp.float32: potential energy value for this configuration x
+    """
+    return 0.5 * jnp.sum(jnp.square(omega * x))
+
+
+def make_harmonic_oscillator_local_energy(
+    omega: jnp.float32, log_psi: Callable[[P, jnp.ndarray], jnp.ndarray]
+) -> Callable[[P, jnp.ndarray], jnp.ndarray]:
+    """Factory to create a local energy fn for the harmonic oscillator log|psi|.
+
+    Args:
+        omega (jnp.float32): spring constant for the harmonic oscillator
+        log_psi (Callable): function which evaluates log|psi| for a harmonic oscillator
+            model wavefunction psi. Has the signature (params, x) -> log|psi(x)|.
+
+    Returns:
+        Callable: local energy function with the signature (params, x) -> local energy
+        associated to the wavefunction psi
+    """
+    grad_log_psi = jax.grad(log_psi, argnums=1)
+
+    def local_energy(params, x):
+        kinetic = -0.5 * laplacian_psi_over_psi(grad_log_psi, params, x)
+        potential = harmonic_oscillator_potential(omega, x)
+        return kinetic + potential
+
+    return jax.vmap(local_energy, in_axes=(None, 0), out_axes=0)
