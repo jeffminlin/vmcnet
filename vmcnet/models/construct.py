c93f888c7dabfb51ef9c057f100fad5931fdc3ce
Jeffmin Lin <jeffmin.lin@gmail.com>
2021-05-26 18:43:01 -0700

    simplified model composition API

--- vmcnet/models/construct.py
+++ vmcnet/models/construct.py
@@ -1,39 +1,23 @@
 """Combine pieces to form full models."""
-from typing import List, Union
+from typing import Callable, Sequence, Union
 
 import flax
 import jax.numpy as jnp
 
 
-def compose_antisymmetry_with_equivariance(equivariance, antisymmetry):
-    """Make a model out of an equivariant part and an antisymmetric part.
+class ComposedModel(flax.linen.Module):
+    """A model made from composable parts.
 
-    Args:
-        equivariance (Callable): equivariant part of the model
-        antisymmetry (Callable): antisymmetric part of the model
-
-    Returns:
-        flax.linen.Module: a flax model which evaluates antisymmetry(equivariance(x)),
-        where x can be split into different spin inputs
+    Attributes:
+        submodels (Sequence[Union[Callable, flax.linen.Module]]): a sequence of
+            functions or flax.linen.Modules which can be composed sequentially
     """
 
-    class AntisymmetricModel(flax.linen.Module):
-        """Model which is antisymmetric via (antisymmetry(equivariance(x))).
+    submodels: Sequence[Union[Callable, flax.linen.Module]]
 
-        Attributes:
-            nspin_split: int or list/array of integers, passed to the
-                `indices_or_sections` arg of jnp.split
-        """
-
-        nspin_split: Union[int, List[int], jnp.ndarray]
-
-        def setup(self):
-            self.equivariance = equivariance
-            self.antisymmetry = antisymmetry
-
-        def __call__(self, x):
-            x_split = jnp.split(x, self.nspin_split, axis=-2)
-            backflow = self.equivariance(x_split)
-            return self.antisymmetry(backflow)
-
-    return AntisymmetricModel
+    @flax.linen.compact
+    def __call__(self, x):
+        outputs = x
+        for model in self.submodels:
+            outputs = model(outputs)
+        return outputs
