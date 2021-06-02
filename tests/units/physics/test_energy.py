4ebe77ccd6f593bfdfc22f51221fc086db0faf26
Jeffmin Lin <jeffmin.lin@gmail.com>
2021-06-01 18:30:17 -0700

    declare log_psi_grad_x

--- tests/units/physics/test_energy.py
+++ tests/units/physics/test_energy.py
@@ -36,14 +36,15 @@ def test_total_energy_grad():
     # log_psi_grad(x) = (grad_a psi / psi)(x) = sum(x^2)
     log_psi_apply = lambda a, x: a * jnp.sum(jnp.square(x), axis=-1)
     a = 3.5
-    x = _make_dummy_x()  # log_psi_grad(x) = [5.0, 25.0, 61.0]
+    x = _make_dummy_x()
+    log_psi_grad_x = jnp.array([5.0, 25.0, 61.0])
     nchains = x.shape[0]
 
     target_local_energies = jnp.array([3.0, 1.0, -1.0])
     target_energy = 1.0
     target_variance = 4.0
     target_grad_energy = 2.0 * jnp.sum(
-        (target_local_energies - target_energy) * jnp.array([5.0, 25.0, 61.0])
+        (target_local_energies - target_energy) * log_psi_grad_x
     )
 
     def local_energy_fn(a, x):
