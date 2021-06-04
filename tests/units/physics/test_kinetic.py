1e328730bd8daa2b8895d2d0eaef967d195df60e
Jeffmin Lin <jeffmin.lin@gmail.com>
2021-06-03 22:03:53 -0700

    fixing import

--- tests/units/physics/test_kinetic.py
+++ tests/units/physics/test_kinetic.py
@@ -1,13 +1,13 @@
 """Test kinetic energy functions."""
 import vmcnet.physics as physics
 
-from .test_core import _make_dummy_log_f, _make_dummy_x
+from ..utils import make_dummy_log_f, make_dummy_x
 
 
 def test_kinetic_energy_shape():
     """Check that the vmapped shape is correct."""
-    _, log_f = _make_dummy_log_f()
-    x = _make_dummy_x()
+    _, log_f = make_dummy_log_f()
+    x = make_dummy_x()
 
     kinetic_energy_fn = physics.kinetic.create_continuous_kinetic_energy(log_f)
     kinetic_energies = kinetic_energy_fn(None, x)
