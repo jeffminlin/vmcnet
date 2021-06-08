cda1faa3711f7dbfe2a546eee18c90a76b3038ef
Jeffmin Lin <jeffmin.lin@gmail.com>
2021-06-08 14:58:53 -0700

    simplify dense layer

--- vmcnet/models/core.py
+++ vmcnet/models/core.py
@@ -1,8 +1,5 @@
 """Core model building parts."""
-from typing import Any
-
 import flax
-from jax import lax
 import jax.numpy as jnp
 
 import vmcnet.utils as utils
@@ -23,9 +20,6 @@ class Dense(flax.linen.Module):
         features (int): the number of output features.
         use_bias (bool, optional): whether to add a bias to the output. Defaults to
             True.
-        dtype (dtype, optional): the dtype of the computation. Defaults to jnp.float32.
-        precision (lax.Precision, optional): numerical precision of the computation. See
-            `jax.lax.Precision` for details. Defaults to None.
         kernel_init (WeightInitializer, optional): initializer function for the weight
             matrix. Defaults to orthogonal initialization.
         bias_init (WeightInitializer, optional): initializer function for the bias.
@@ -35,11 +29,9 @@ class Dense(flax.linen.Module):
     """
 
     features: int
-    use_bias: bool = True
-    dtype: Any = jnp.float32
-    precision: Any = None
     kernel_init: WeightInitializer = get_kernel_initializer("orthogonal")
     bias_init: WeightInitializer = get_bias_initializer("normal")
+    use_bias: bool = True
     register_kfac: bool = True
 
     @flax.linen.compact
@@ -47,29 +39,21 @@ class Dense(flax.linen.Module):
         """Applies a linear transformation to the inputs along the last dimension.
 
         Args:
-          inputs: The nd-array to be transformed.
+            inputs: The nd-array to be transformed.
 
         Returns:
-          The transformed input.
+            The transformed input.
         """
-        inputs = jnp.asarray(inputs, self.dtype)
         kernel = self.param(
             "kernel", self.kernel_init, (inputs.shape[-1], self.features)
         )
-        kernel = jnp.asarray(kernel, self.dtype)
-        y = lax.dot_general(
-            inputs,
-            kernel,
-            (((inputs.ndim - 1,), (0,)), ((), ())),
-            precision=self.precision,
-        )
+        y = jnp.dot(inputs, kernel)
         bias = None
         if self.use_bias:
             bias = self.param("bias", self.bias_init, (self.features,))
-            bias = jnp.asarray(bias, self.dtype)
             y = y + bias
 
         if self.register_kfac:
-            return utils.kfac.register_dense(y, inputs, kernel, bias)
+            return utils.kfac.register_batch_dense(y, inputs, kernel, bias)
         else:
             return y
