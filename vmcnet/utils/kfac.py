2eba3008a5f5712bf5cc1fad9f422ce2880550d8
Jeffmin Lin <jeffmin.lin@gmail.com>
2021-06-08 14:58:42 -0700

    better names for kfac tagging

--- vmcnet/utils/kfac.py
+++ vmcnet/utils/kfac.py
@@ -3,45 +3,44 @@
 These functions are taken directly from DeepMind's FermiNet jax branch, see
 https://github.com/deepmind/ferminet/blob/aade61b3d30883b3238d6b50c85404d0e8176155/ferminet/curvature_tags_and_blocks.py
 
-Some names are slightly modified (e.g. repeated_dense -> dense).
+Some names are slightly modified (e.g. repeated_dense -> batch_dense).
 """
 from typing import Optional, Mapping, Union
 
-import jax
 import jax.numpy as jnp
 
 from kfac_ferminet_alpha import curvature_blocks as blocks
 from kfac_ferminet_alpha import layers_and_loss_tags as tags
-from kfac_ferminet_alpha import utils
+from kfac_ferminet_alpha import utils as kfac_utils
 
 
-vmap_matmul = jax.vmap(jnp.matmul, in_axes=(0, 0), out_axes=0)
-
-dense_tag = tags.LayerTag("dense_tag", 1, 1)
+batch_dense_tag = tags.LayerTag("batch_dense_tag", 1, 1)
 
 
-def register_dense(y, x, w, b):
+def register_batch_dense(y, x, w, b):
     """Register the weights of a dense layer."""
     if b is None:
-        return dense_tag.bind(y, x, w)
-    return dense_tag.bind(y, x, w, b)
+        return batch_dense_tag.bind(y, x, w)
+    return batch_dense_tag.bind(y, x, w, b)
 
 
-class DenseBlock(blocks.DenseTwoKroneckerFactored):
-    """Dense block."""
+class BatchDenseBlock(blocks.DenseTwoKroneckerFactored):
+    """Dense curvature block corresponding to inputs of shape (..., d)."""
 
     def compute_extra_scale(self) -> Optional[jnp.ndarray]:
+        """Extra scale factor for the curvature block (relative to other blocks)."""
         (x_shape,) = self.inputs_shapes
-        return utils.product(x_shape) // (x_shape[0] * x_shape[-1])
+        return kfac_utils.product(x_shape) // (x_shape[0] * x_shape[-1])
 
     def update_curvature_matrix_estimate(
         self,
-        info: Mapping[str, blocks._Arrays],  # pylint: disable=protected-access
+        info: Mapping[str, blocks._Arrays],
         batch_size: int,
         ema_old: Union[float, jnp.ndarray],
         ema_new: Union[float, jnp.ndarray],
         pmap_axis_name: str,
     ) -> None:
+        """Satsify kfac_ferminet_alpha's assumption that the inputs are 2d."""
         info = dict(**info)
         (x,), (dy,) = info["inputs"], info["outputs_tangent"]
         assert x.shape[0] == batch_size
@@ -52,4 +51,4 @@ class DenseBlock(blocks.DenseTwoKroneckerFactored):
         )
 
 
-blocks.set_default_tag_to_block("dense_tag", DenseBlock)
+blocks.set_default_tag_to_block("batch_dense_tag", BatchDenseBlock)
