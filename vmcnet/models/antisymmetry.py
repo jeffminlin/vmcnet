aefe65c20aeeceb91d5642995203c1213acfe2d7
Jeffmin Lin <jeffmin.lin@gmail.com>
2021-06-17 17:38:12 -0700

    multiple fns to antisymmetrize for split

--- vmcnet/models/antisymmetry.py
+++ vmcnet/models/antisymmetry.py
@@ -1,7 +1,7 @@
 """Antisymmetry parts to compose into a model."""
 import functools
 import itertools
-from typing import Callable, Tuple
+from typing import Any, Callable, Tuple
 
 import flax
 import jax
@@ -95,18 +95,42 @@ class ParallelPermutations(flax.linen.Module):
     n: int
 
     def setup(self):
+        """Store the list of permutations and signs for the symmetric group."""
         self.permutation_list = list(itertools.permutations(range(self.n)))
         self.signs = _get_lexicographic_signs(self.n)
 
     def __call__(self, x: jnp.ndarray) -> Tuple[jnp.ndarray, jnp.ndarray]:
+        """Collect all permutations of x and the signs of these permutations.
+
+        Args:
+            x (jnp.ndarray): an array of particles with shape (..., n, d)
+
+        Returns:
+            (jnp.ndarray, jnp.ndarray): all permutations of x along the second axis,
+            with shape (..., n!, n, d), and the signs of the permutations in the same
+            order as the third-to-last axis (lexicographic order)
+        """
         return jnp.take(x, self.permutation_list, axis=-2), self.signs
 
 
 class SplitBruteForceAntisymmetrize(flax.linen.Module):
-    fn_to_antisymmetrize: Callable
+    """Separately antisymmetrize fns over leaves of a pytree and return the product.
+
+    Attributes:
+        fns_to_antisymmetrize (pytree): pytree of functions with the same tree structure
+            as the input pytree, each of which is a Callable with signature
+            jnp.ndarray of shape (..., d) -> jnp.ndarray of shape (..., 1)
+        logabs (bool, optional): whether to compute sum_i log(abs(psi_i)) if logabs is
+            True, or prod_i psi_i if logabs is False, where psi_i is the output from
+            antisymmetrizing the ith function on the ith input. Defaults to True.
+    """
+
+    fns_to_antisymmetrize: Any  # pytree of Callables with the same treedef as input
     logabs: bool = True
 
-    def _single_leaf_call(self, x: jnp.ndarray) -> jnp.ndarray:
+    def _single_leaf_call(
+        self, fn_to_antisymmetrize: Callable[[jnp.ndarray], jnp.ndarray], x: jnp.ndarray
+    ) -> jnp.ndarray:
         n = x.shape[-2]
         d = x.shape[-1]
 
@@ -115,14 +139,25 @@ class SplitBruteForceAntisymmetrize(flax.linen.Module):
         signs = jnp.expand_dims(signs, axis=-1)  # (n!, 1)
 
         # perms_out has shape (..., n!, 1)
-        perms_out = self.fn_to_antisymmetrize(x_perm)
+        perms_out = fn_to_antisymmetrize(x_perm)
         signed_perms_out = signs * perms_out
 
         return jnp.sum(signed_perms_out, axis=(-1, -2))
 
     @flax.linen.compact
     def __call__(self, xs) -> jnp.ndarray:
-        antisyms = jax.tree_map(self._single_leaf_call, xs)
+        """Antisymmetrize the leaves of self.fns_to_antisymmetrize on the leaves of xs.
+
+        Args:
+            xs (pytree): pytree of inputs with the same tree structure as that of
+                self.fns_to_antisymmetrize
+
+        Returns:
+            jnp.ndarray: sum_i log(abs(psi_i)) if self.logabs is True, or prod_i psi_i
+            if self.logabs is False, where psi_i is the output from antisymmetrizing the
+            ith function on the ith input.
+        """
+        antisyms = jax.tree_map(self._single_leaf_call, self.fns_to_antisymmetrize, xs)
         if not self.logabs:
             return _reduce_prod_over_leaves(antisyms)
 
@@ -131,8 +166,31 @@ class SplitBruteForceAntisymmetrize(flax.linen.Module):
 
 
 class ComposedBruteForceAntisymmetrize(flax.linen.Module):
-    fn_to_antisymmetrize: Callable
+    """Antisymmetrize a function over the leaves of a pytree.
+
+    For each leaf of a pytree, a given function of all the leaves is antisymmetrized
+    over the second-to-last axis of each leaf. These brute-force antisymmetrization
+    operations are composed with each other (they commute, so the order does not
+    matter), giving an output which is antisymmetric with respect to particle exchange
+    within each leaf but not with respect to particle exchange between leaves.
+
+    Attributes:
+        fn_to_antisymmetrize (Callable): Callable with signature
+            jnp.ndarray with shape (..., d) -> (..., 1). This is the function to be
+            antisymmetrized.
+        logabs (bool, optional): whether to compute log(abs(psi)) if logabs is True, or
+            psi if logabs is False, where psi is the output from antisymmetrizing
+            self.fn_to_antisymmetrize. Defaults to True.
+    """
+
+    fn_to_antisymmetrize: Callable[[jnp.ndarray], jnp.ndarray]
     logabs: bool = True
+
+    def setup(self):
+        """Setup the function to antisymmetrize."""
+        # workaround MyPy's typing error for callable attribute, see
+        # https://github.com/python/mypy/issues/708
+        self._fn_to_antisymmetrize = self.fn_to_antisymmetrize
 
     def _get_single_leaf_perm(self, x: jnp.ndarray) -> Tuple[jnp.ndarray, jnp.ndarray]:
         n = x.shape[-2]
@@ -140,6 +198,17 @@ class ComposedBruteForceAntisymmetrize(flax.linen.Module):
 
     @flax.linen.compact
     def __call__(self, xs) -> jnp.ndarray:
+        """Antisymmetrize self.fn_to_antisymmetrize over the leaves of xs.
+
+        Args:
+            xs (pytree): a pytree of inputs, each which corresponds to a different set
+                of particles to antisymmetrize with respect to
+
+        Returns:
+            jnp.ndarray: log(abs(psi)) if self.logabs is True, or psi if logabs is
+            False, where psi is the output from antisymmetrizing
+            self.fn_to_antisymmetrize on all leaves of xs.
+        """
         perms_and_signs = jax.tree_map(self._get_single_leaf_perm, xs)
         perms_and_signs_leaves, _ = jax.tree_flatten(perms_and_signs, _istupleofarrays)
         nspins = len(perms_and_signs_leaves)
@@ -172,7 +241,7 @@ class ComposedBruteForceAntisymmetrize(flax.linen.Module):
         concat_perms = jnp.reshape(concat_perms, concat_perms.shape[:-2] + (-1,))
 
         # all_perms_out has shape (..., n_1!, ..., n_k!, 1)
-        all_perms_out = self.fn_to_antisymmetrize(concat_perms)
+        all_perms_out = self._fn_to_antisymmetrize(concat_perms)
         signed_perms_out = _reduce_prod_over_leaves([all_perms_out, reshaped_signs])
 
         antisymmetrized_out = jnp.sum(
