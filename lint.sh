6dad318ec517c67a9f60e7dce16023dcb8690805
Gil Goldshlager <=>
2021-07-06 17:28:18 -0700

    Update lint.sh to not check files in notes, etc

--- lint.sh
+++ lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Running mypy to check for type errors"
-mypy .
+mypy vmcnet tests setup.py
 echo "Running flake8 to check for code style errors"
-flake8 --max-line-length=88 --select=C,E,F,W,B,B950 --ignore=E203,W503 --per-file-ignores="__init__.py:F401"
+flake8 --filename='./vmcnet/*.py','./tests/*.py','./setup.py' --max-line-length=88 --select=C,E,F,W,B,B950 --ignore=E203,W503 --per-file-ignores="__init__.py:F401"
 echo "Running pydocstyle to check for docstring style errors"
-pydocstyle --match='(.)*\.py' --add-ignore=D401
+pydocstyle ./vmcnet ./tests ./setup.py --match='(.)*\.py' --add-ignore=D401
