a9bf4d638d4feb4e871d080b1832b2b016802f3d
Gil Goldshlager <=>
2021-08-03 18:25:49 -0400

    Test sequence and int hidden particles inputs

--- tests/units/models/test_construct.py
+++ tests/units/models/test_construct.py
@@ -104,33 +104,32 @@ def _make_ferminets():
     return key, init_pos, log_psis
 
 
-def _make_embedded_slave_ferminet():
+def _make_embedded_slave_ferminets():
     key, ion_pos, init_pos, spin_split, ndense_list = _get_initial_pos_and_hyperparams()
 
     log_psis = []
     cyclic_spins = False
     backflow = _get_backflow(spin_split, ndense_list, cyclic_spins, ion_pos)
-    invariance_backflow = (
-        _get_backflow(spin_split, ndense_list, cyclic_spins, ion_pos),
-    )
-    nhidden_particles_per_spin = (2, 3)
+    invariance_backflow = _get_backflow(spin_split, ndense_list, cyclic_spins, ion_pos)
+    nhidden_particles_per_spins = [(2, 3), 2]
 
-    log_psi = models.construct.EmbeddedSlaveFermiNet(
-        spin_split,
-        nhidden_particles_per_spin,
-        backflow,
-        3,
-        models.weights.get_kernel_initializer("he_normal"),
-        models.weights.get_kernel_initializer("lecun_normal"),
-        models.weights.get_kernel_initializer("ones"),
-        models.weights.get_bias_initializer("uniform"),
-        invariance_backflow=invariance_backflow,
-        invariance_kernel_initializer=models.weights.get_kernel_initializer(
-            "he_normal"
-        ),
-        invariance_bias_initializer=models.weights.get_bias_initializer("uniform"),
-    )
-    log_psis.append(log_psi)
+    for nhidden_particles_per_spin in nhidden_particles_per_spins:
+        log_psi = models.construct.EmbeddedSlaveFermiNet(
+            spin_split,
+            nhidden_particles_per_spin,
+            backflow,
+            3,
+            models.weights.get_kernel_initializer("he_normal"),
+            models.weights.get_kernel_initializer("lecun_normal"),
+            models.weights.get_kernel_initializer("ones"),
+            models.weights.get_bias_initializer("uniform"),
+            invariance_backflow=invariance_backflow,
+            invariance_kernel_initializer=models.weights.get_kernel_initializer(
+                "he_normal"
+            ),
+            invariance_bias_initializer=models.weights.get_bias_initializer("uniform"),
+        )
+        log_psis.append(log_psi)
 
     return key, init_pos, log_psis
 
@@ -253,13 +252,13 @@ def test_ferminet_can_be_evaluated():
 
 def test_embedded_slave_ferminet_can_be_constructed():
     """Check construction of EmbeddedSlaveFermiNet does not fail."""
-    _make_embedded_slave_ferminet()
+    _make_embedded_slave_ferminets()
 
 
 @pytest.mark.slow
 def test_embedded_slave_ferminet_can_be_evaluated():
     """Check evaluation of EmbeddedSlaveFermiNet does not fail."""
-    key, init_pos, log_psis = _make_embedded_slave_ferminet()
+    key, init_pos, log_psis = _make_embedded_slave_ferminets()
     (_jit_eval_model(key, init_pos, log_psi) for log_psi in log_psis)
 
 
