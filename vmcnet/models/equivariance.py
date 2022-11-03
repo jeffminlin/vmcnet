2a074521663795ff616a7d47f72ebed2fa0d5a06
JiahaoYao <jiahaoyao.math@gmail.com>
2022-11-03 16:32:07 -0700

    fix the typo

--- vmcnet/models/equivariance.py
+++ vmcnet/models/equivariance.py
@@ -412,8 +412,8 @@ class FermiNetOneElectronLayer(Module):
 
         in_1e_up, in_1e_down = jnp.split(in_1e, splits, axis=axis)
 
-        in_2e_up = self._ln1(self._general_attn_up(in_1e_up, in_1e) + in_1e_up)
-        in_2e_down = self._ln2(self._general_attn_down(in_1e_down, in_1e) + in_1e_down)
+        in_2e_up = self._layernorm1(self._general_attn_up(in_1e_up, in_1e) + in_1e_up)
+        in_2e_down = self._layernorm2(self._general_attn_down(in_1e_down, in_1e) + in_1e_down)
         concat_2e = [in_2e_up, in_2e_down]
         all_spins = jnp.concatenate(concat_2e, axis=-2)
 
