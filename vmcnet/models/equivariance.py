946eadd262bfa9e91087f5ede457eb003902135a
Gil Goldshlager <=>
2023-04-20 17:29:08 -0700

    Tweak to decay only exponentially

--- vmcnet/models/equivariance.py
+++ vmcnet/models/equivariance.py
@@ -686,7 +686,9 @@ def _compute_exponential_envelopes_on_leaf(
         )
     # scale_out has shape (..., nelec, norbitals, nion, d)
     distances = jnp.linalg.norm(scale_out, axis=-1)
-    inv_exp_distances = jnp.exp(-(distances**2))  # (..., nelec, norbitals, nion)
+    inv_exp_distances = jnp.exp(
+        -jnp.sqrt(distances**2 + 1)
+    )  # (..., nelec, norbitals, nion)
 
     # Multiply elementwise over final two axes and sum over final axis, returning
     # (..., nelec, norbitals)
