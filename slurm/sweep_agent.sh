2869889e902dbc2cd2fe7afb886f914e12567d05
Gil <ggoldshlager@gmail.com>
2026-04-09 11:19:29 -0700

    Fix slurm scripts

--- slurm/sweep_agent.sh
+++ slurm/sweep_agent.sh
@@ -2,17 +2,20 @@
 #SBATCH --job-name=vmc-sweep
 #SBATCH --account=co_esmath
 #SBATCH --partition=savio3_gpu
-#SBATCH --nodes=1
+#
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:GTX2080TI:1
+#
 #SBATCH --time=24:00:00
-#SBATCH --output=./slurm_out/%A_%a_%x.out
+#SBATCH --output="/global/scratch/users/ggoldshlager/logs/sweeps/out"
+#SBATCH --error="/global/scratch/users/ggoldshlager/logs/sweeps/error"
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=ggoldsh@berkeley.edu
 
-module load python
-module load cuda/10.2
-conda activate vmcnet
-export XLA_FLAGS=--xla_gpu_cuda_data_dir=/global/software/sl-7.x86_64/modules/langs/cuda/10.2
+source activate vmcnet
+
+cd /global/home/groups/co_esmath/ggoldshlager/vmcnet
 
 # SWEEP_PATH is the full wandb path: entity/project/sweep_id
 # e.g. ggoldsh-university-of-california-berkeley/preconditioning/abc123
